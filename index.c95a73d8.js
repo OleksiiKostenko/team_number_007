bbeb7ced3aa8e3205d4890451bae04f40cd717be
OleksiiKostenko <110487805+OleksiiKostenko@users.noreply.github.com>
2022-11-02 17:57:13 +0000

    Deploying to gh-pages from @ OleksiiKostenko/team_number_007@e6d325c8c111a5a319ae191253c67fddec4f88bd 🚀

--- index.c95a73d8.js
+++
@@ -1,2 +0,0 @@
-$(".autoplay").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3});
-//# sourceMappingURL=index.c95a73d8.js.map
---
+++ index.e5e0416c.js
@@ -0,0 +1,2 @@
+$(".autoplay").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3,prevArrow:$(".slaider__btn--left"),nextArrow:$(".slaider__btn--rigth")});
+//# sourceMappingURL=index.e5e0416c.js.map
