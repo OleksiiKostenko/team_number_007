79065ed6726f421d4d003f25342c31a2a0518533
SergSmilya <sergsmilichenko@gmail.com>
2022-11-02 19:52:39 +0200

    add slider button

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,8 @@
-$('.autoplay').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+// $('.autoplay').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+
+$(".autoplay").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3, prevArrow: $('.slaider__btn--left'), nextArrow: $('.slaider__btn--rigth')});
