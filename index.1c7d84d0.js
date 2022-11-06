d639fbf150e3a40a6bf05fbb27f8a2eef897b315
OleksiiKostenko <110487805+OleksiiKostenko@users.noreply.github.com>
2022-11-06 14:45:01 +0000

    Deploying to gh-pages from @ OleksiiKostenko/team_number_007@cf68e719ac44d914547973cacb532c234c1be1a3 🚀

---
+++ index.1c7d84d0.js
@@ -0,0 +1,2 @@
+AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:50,throttleDelay:99,offset:120,delay:0,duration:400,easing:"ease",once:!1,mirror:!1,anchorPlacement:"top-bottom"});
+//# sourceMappingURL=index.1c7d84d0.js.map
